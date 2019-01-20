b19dc7c3a9133f5974aa9bac4b7830309b213cfe
ziq358 <461804160@qq.com>
2019-01-20 18:18:25 +0800

    使用 Masonry 库 进行约束

--- ZQPlayerIOS/controller/video/VideoListViewController.m
+++ ZQPlayerIOS/controller/video/VideoListViewController.m
@@ -26,36 +26,34 @@
     headerView.translatesAutoresizingMaskIntoConstraints=NO;
     headerView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:headerView];
-    //左边 右边距
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-0-[headerView]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(headerView)]];
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[headerView]" options:0 metrics:nil views:NSDictionaryOfVariableBindings(headerView)]];
+    [headerView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(self.view);
+        make.right.mas_equalTo(self.view);
+    }];
     
     UIView* searchView = [[UIView alloc] init];
     searchView.translatesAutoresizingMaskIntoConstraints=NO;
     searchView.backgroundColor = [UIColor colorWithHexString:@"#f4f4f4" alpha:1.0];
     searchView.layer.cornerRadius = 15;
     [headerView addSubview:searchView];
-    [headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-15-[searchView]-15-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(searchView)]];
-    [headerView addConstraint:[NSLayoutConstraint constraintWithItem:searchView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:30]];
     
-    
+    [searchView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(headerView).mas_offset(10);
+        make.left.mas_equalTo(headerView).mas_offset(15);
+        make.right.mas_equalTo(headerView).mas_offset(-15);
+        make.height.mas_equalTo(30);
+    }];
     
     NSArray *images = @[@"banner1",@"banner2",@"banner3",@"banner4"];
     WMLoopView *bannerView = [[WMLoopView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 150) images:images autoPlay:YES delay:5.0];
     bannerView.translatesAutoresizingMaskIntoConstraints=NO;
     [headerView addSubview:bannerView];
     
-    [headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-0-[bannerView]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(bannerView)]];
-    
-    [headerView addConstraint:[NSLayoutConstraint constraintWithItem:searchView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:headerView attribute:NSLayoutAttributeTop multiplier:1 constant:10]];
-    
-    [headerView addConstraint:[NSLayoutConstraint constraintWithItem:bannerView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:searchView attribute:NSLayoutAttributeBottom multiplier:1 constant:10]];
-    
-    [headerView addConstraint:[NSLayoutConstraint constraintWithItem:bannerView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:headerView attribute:NSLayoutAttributeBottom multiplier:1 constant:-10]];
-    
-    //searchView 根据 子view 的高度适应， searchView不能设置高度，否则无效
-    [headerView addConstraint:[NSLayoutConstraint constraintWithItem:bannerView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:150]];
-
+    [bannerView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo([searchView mas_bottom]).mas_offset(10);
+        make.bottom.mas_equalTo([headerView mas_bottom]).mas_offset(-10);
+        make.height.mas_equalTo(150);
+    }];
     
 }
 
