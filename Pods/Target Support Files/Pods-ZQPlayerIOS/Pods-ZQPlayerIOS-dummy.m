871af841cbaefa7ebd0d6a00224cd05e8fb56f2f
wuyanqiang <461804160@qq.com>
2019-03-07 18:51:37 +0800

    tianija

---
+++ Pods/Target Support Files/Pods-ZQPlayerIOS/Pods-ZQPlayerIOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZQPlayerIOS : NSObject
+@end
+@implementation PodsDummy_Pods_ZQPlayerIOS
+@end
---
+++ Pods/Target Support Files/Pods-ZQPlayerIOSUITests/Pods-ZQPlayerIOSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZQPlayerIOSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ZQPlayerIOSUITests
+@end
